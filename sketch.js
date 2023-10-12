7e92f2287ef1cfa754250fff9bb6eb01988586b4
riya khattri <82651191+riya-khattri@users.noreply.github.com>
2023-10-12 15:57:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  var numS = (width*height)/(20*20)
+  
+  var r=0;
+  var g=50;
+  var b=255;
+  
+  for(var i=0; i < numS; i+=40)
+    {
+      for(var j=0; j<numS; j+=40)
+        {
+          noStroke()
+          fill(r,g,b);
+          square(i,j,40);
+          r+=40;
+          
+        }
+      r=0;
+      g+=25;
+      b-=20;
+    }
+  
+}
